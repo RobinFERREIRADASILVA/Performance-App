3717cc970eb689dcd5a5d7272dce70a20302c151
RobinFERREIRADASILVA <ferreiradasilvarobin@gmail.com>
2022-09-13 23:00:58 +0200

    fix cross axis aligment

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -12,10 +12,30 @@ class Home extends StatelessWidget {
       appBar: AppBarMenu(),
       body: SingleChildScrollView(
         child: Column(
-          children: const [
-            Padding(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
               padding: EdgeInsets.fromLTRB(20, 30, 0, 0),
-              child: Text('Vos séances'),
+              child: Text(
+                'Vos séances',
+                textAlign: TextAlign.left,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: 200,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: <Widget>[
+                  Container(
+                    height: 200,
+                    width: 200,
+                    color: Colors.red,
+                  )
+                ],
+              ),
             )
           ],
         ),
